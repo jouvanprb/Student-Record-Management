2329af1421e0be7903bac3cae97fd4c5fdb5fe13
Vann <ovanpurba005@gmail.com>
2026-07-09 00:42:01 +0700

    feat: add load student function (#1)

--- file.go
+++ file.go
@@ -32,3 +32,32 @@ func CreateFileIfNotExist(){
 	// Success create file message
 	fmt.Printf("File %v has been created successfully \n", fileName)
 }
+
+func LoadStudent() []student {
+	// Open file
+	file, err := os.OpenFile("data.json", os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Printf("Return Error: %v\n", err)
+		return nil
+	}
+	defer file.Close()
+
+	// Read file
+	byte, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Printf("Return Error %v\n", err)
+		return nil
+	}
+
+	// Unmarshall JSON
+	var students []student
+	err = json.Unmarshal(byte, &students)
+	if err != nil {
+		fmt.Printf("Error parsing JSON %v\n", err)
+		return nil
+	}
+
+	// print
+	fmt.Println(students)
+	return students
+}
