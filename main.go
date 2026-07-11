ce4259e342c69cf5540e7d0a7dc660e4ade071a3
jouvanprb <ovanpurba005@gmail.com>
2026-07-11 20:02:48 +0700

    feat: update final for main functionality

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 		ShowMenu()
 
 		var choice int
-		fmt.Print("Choose menu (1-6): ")
+		fmt.Print("  Choose menu (1-6): ")
 		fmt.Scan(&choice)
 		fmt.Println(strings.Repeat("─", 35))
 
@@ -21,10 +21,19 @@ func main() {
 			AddStudent()
 		case 2:
 			ViewStudent()
+		case 3:
+			SearchStudent()
+		case 4:
+			UpdateStudent()
+		case 5:
+			DeleteStudent()
 		case 6:
-			fmt.Println("-> PROGRAM STOPPED!")
+			fmt.Println(" ==> Program Stopped!")
 			fmt.Println()
 			return
+		default:
+			fmt.Println(" ==> Wrong No. Please Input from (1-6)")
+			continue
 		}
 	}
 
